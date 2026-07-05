dae611393c4a239029cac3e4ff62c91079d38e9b
Aseel <aswahaibi8@gmail.com>
2026-07-05 17:45:17 +0400

    task 2 - completed

--- SparkToCode26/Program.cs
+++ SparkToCode26/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace SparkToCode26;
 
-class Program-
+class Program
 {
     static void Main(string[] args)
     {
         //Task 1 - Absolute Difference
+        /*
         Console.WriteLine("enter the first number: ");
         int a = int.Parse(Console.ReadLine());
         Console.WriteLine("enter the second number: ");
@@ -14,6 +15,20 @@ class Program-
         decimal abs = Math.Abs(sub);
         Console.WriteLine("Subtraction resukt is: " + sub);
         Console.WriteLine("Absolute value :  "+abs);
+        */
         
+        //Task 2 - Power & Root Explorer
+        
+        Console.WriteLine("enter a number:  ");
+        int a = int.Parse(Console.ReadLine());
+        
+        double p = Math.Pow(a, 2);
+        double s = Math.Sqrt (a);
+        
+        Console.WriteLine ("The power of " + a + " is " + p);
+        Console.WriteLine ("the square root of  " +a+ " is " +  s);
+        
+        
+       
     }
 }
