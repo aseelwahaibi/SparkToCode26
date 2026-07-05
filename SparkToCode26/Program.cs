0b6fb9094f7a03d76e8a9a4d3b0287f5594c8439
Aseel <aswahaibi8@gmail.com>
2026-07-05 17:14:37 +0400

    task 1 - solved

--- SparkToCode26/Program.cs
+++ SparkToCode26/Program.cs
@@ -1,11 +1,19 @@
 ﻿namespace SparkToCode26;
 
-class Program
+class Program-
 {
     static void Main(string[] args)
     {
         //Task 1 - Absolute Difference
+        Console.WriteLine("enter the first number: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the second number: ");
+        int b = int.Parse(Console.ReadLine());
         
+        int sub = a - b;
+        decimal abs = Math.Abs(sub);
+        Console.WriteLine("Subtraction resukt is: " + sub);
+        Console.WriteLine("Absolute value :  "+abs);
         
     }
 }
