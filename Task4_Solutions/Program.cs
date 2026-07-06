705dfab8dd34985d2dad10fbfca5f5744e853614
Aseel <aswahaibi8@gmail.com>
2026-07-06 23:04:05 +0400

    part 4 task 1 - solved

---
+++ Task4_Solutions/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task4_Solutions;
+
+class Program
+{
+    public static void PrintWelcome(string name)
+    {
+        Console.WriteLine("Welcome "+name);
+    }
+    
+    
+    static void Main(string[] args)
+    {
+        PrintWelcome("Aseel");
+    }
+}
