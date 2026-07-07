d07229c70c2ee365a3a4e011f8e3ee68c69d1772
Aseel <aswahaibi8@gmail.com>
2026-07-07 23:05:27 +0400

    part 5 task 1 - solved

---
+++ Task5_solutions/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task5_solutions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     //Task 1 - Fixed Grades Array
+     int[] grades = new int[4]; 
+      grades[0] = 90;
+      grades[1] = 85;
+      grades[2] = 70;
+      grades[3] = 60;
+
+      int counter = 0;
+      for (counter = 0; counter < 4; counter++) //Input and store the grades.
+      {
+          Console.Write("Enter grade " + (counter + 1) + ": ");
+          grades[counter] = int.Parse(Console.ReadLine());
+          
+          foreach (int grade in grades) //Read and print the grades.
+          {
+              Console.WriteLine(grade);
+          }
+      }
+
+    }
+}
