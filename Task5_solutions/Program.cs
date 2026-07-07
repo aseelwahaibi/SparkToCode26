0325385ade7f255eec0f69c03773cf2bd68aa895
Aseel <aswahaibi8@gmail.com>
2026-07-07 23:28:32 +0400

    part 5 task 2 - solved

--- Task5_solutions/Program.cs
+++ Task5_solutions/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
      //Task 1 - Fixed Grades Array
+     /*
      int[] grades = new int[4]; 
       grades[0] = 90;
       grades[1] = 85;
@@ -22,6 +23,33 @@ class Program
               Console.WriteLine(grade);
           }
       }
+*/
+     
+     
+     
+     ///////////////////////////////////////
+     
+     //Task 2 - Dynamic To-Do List
+     
+     List<string> todo = new List<string>();
+     todo.Add("finish today tasks");
+     todo.Add("watch recorded session");
+     todo.Add("practice tasks");
 
+     int counter = 0; 
+     
+     for (counter = 0; counter < 4; counter++) 
+     {
+         Console.Write("enter 5 tasks  " + (counter + 1) + ": ");
+         string task = Console.ReadLine();
+         todo.Add(task); 
+         
+         
+     }
+     foreach (string task in todo) 
+     {
+         Console.WriteLine("- "+task);
+     }
+     
     }
 }
